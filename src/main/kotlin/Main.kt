c1a41d77b1f050e8d93c141f3aaec4e53b3f0d76
Ja-woon <leon320510@gmail.com>
2022-11-26 18:43:11 +0900

    hello

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,9 +9,7 @@ fun main(args: Array<String>) {
         material = arrayListOf<String>("물500ml", "냄비"),
         description = "물 550ml를 냄비에 받은 후 4분동안 끓이세요",
         duration = TimeUnit.MINUTES.toMillis(4),
-        canDoOther = true,
-        left = false,
-        right = true
+        canDoOther = true
     );
 
     var ramen_recipe2 = Recipe(
@@ -19,10 +17,14 @@ fun main(args: Array<String>) {
         material = arrayListOf<String>("라면 1봉지"),
         description = "면, 스프, 후레이크를 넣어주세요. 4분 더 끓이신 후 완성입니다.",
         duration = TimeUnit.MINUTES.toMillis(4),
-        canDoOther = false,
-        left = true,
-        right = false
+        canDoOther = false
     );
+
+    ramen_recipe1.left = null
+    ramen_recipe1.right = ramen_recipe2
+
+    ramen_recipe2.left = ramen_recipe1
+    ramen_recipe1.right = null
 
     ramen_recipe.add(ramen_recipe1);
     ramen_recipe.add(ramen_recipe2);
@@ -37,9 +39,7 @@ fun main(args: Array<String>) {
         material = arrayListOf<String>("후라이펜", "소량의 기름"),
         description = "후라이팬에 기름을 두리고 1분간 예열하세요",
         duration = TimeUnit.MINUTES.toMillis(1),
-        canDoOther = true,
-        left = false,
-        right = true
+        canDoOther = true
     );
 
     var steak_recipe2 = Recipe(
@@ -47,10 +47,15 @@ fun main(args: Array<String>) {
         material = arrayListOf<String>("소고기 400g"),
         description = "후라이팬에 소고기를 올리고 취향에 맞게 (2분 - 레어, 3분 미디엄 레어, 4분 미디엄) 익혀주시면 완성입니다.",
         duration = TimeUnit.SECONDS.toMinutes(4),
-        canDoOther = false,
-        left = true,
-        right = false
+        canDoOther = false
     );
+
+    steak_recipe1.left = null
+    steak_recipe1.right = steak_recipe2
+
+    steak_recipe2.left = steak_recipe1
+    steak_recipe2.right = null
+
 
     steak_recipe.add(steak_recipe1);
     steak_recipe.add(steak_recipe2);
@@ -65,28 +70,33 @@ fun main(args: Array<String>) {
         material = arrayListOf<String>("후라이펜", "소량의 기름"),
         description = "후라이팬에 기름을 두리고 1분간 예열하세요",
         duration = TimeUnit.MINUTES.toMillis(1),
-        canDoOther = true,
-        left = false,
-        right = true
+        canDoOther = true
     );
     var egg_fried_rice_recipe2 = Recipe(
         title = "계란 익히기",
         material = arrayListOf<String>("계란 2알"),
         description = "후라이팬에서 계란을 튀기듯이 2분간 익혀주세요",
         duration = TimeUnit.SECONDS.toMinutes(2),
-        canDoOther = false,
-        left = true,
-        right = true
+        canDoOther = false
     );
     var egg_fried_rice_recipe3 = Recipe(
         title = "밥과 계란 볶기",
         material = arrayListOf<String>("밥 1공기"),
         description = "후라이팬에 밥을 넣고 계란과 함께 3분정도 볶아주시면 완성입니다.",
         duration = TimeUnit.SECONDS.toMinutes(3),
-        canDoOther = false,
-        left = true,
-        right = false
+        canDoOther = false
     );
+
+    egg_fried_rice_recipe1.left = null
+    egg_fried_rice_recipe1.right = egg_fried_rice_recipe2
+
+    egg_fried_rice_recipe2.left = egg_fried_rice_recipe1
+    egg_fried_rice_recipe2.right = egg_fried_rice_recipe3
+
+    egg_fried_rice_recipe3.left = egg_fried_rice_recipe2
+    egg_fried_rice_recipe3.right = null
+
+
 
     egg_fried_rice_recipe.add(egg_fried_rice_recipe1);
     egg_fried_rice_recipe.add(egg_fried_rice_recipe2);
@@ -101,7 +111,18 @@ fun main(args: Array<String>) {
     cookList.add(steak)
     cookList.add(egg_fried_rice)
 
-    var ca = CookingAssistant(cookList)
+    var temp:Recipe = egg_fried_rice_recipe1
+    while(true){
+        println(temp.description)
+        if(temp.right == null){
+            break
+        }
+        else{
+            temp = temp.right!!
+        }
+
+    }
+    //var ca = CookingAssistant(cookList)
 
 
 
--- src/main/kotlin/Recipe.kt
+++ src/main/kotlin/Recipe.kt
@@ -1,3 +1,5 @@
-class Recipe(var title: String, var material: ArrayList<String>, var description: String, var duration:Long, var canDoOther: Boolean, var left: Boolean, var right: Boolean) {
-    //
+class Recipe(var title: String, var material: ArrayList<String>, var description: String, var duration:Long, var canDoOther: Boolean, var left: Recipe?, var right: Recipe?) {
+    constructor(title: String, material: ArrayList<String>, description: String, duration: Long, canDoOther: Boolean) : this(title, material, description, duration, canDoOther, null, null){
+        // 생성자
+    }
 }
